39a366213e459696c6bcaa19308c50d54f7682d5
Sebastien Iooss <archimist.linux@gmail.com>
2021-01-13 14:29:13 +0100

    clean code

--- Readme.md
+++ Readme.md
@@ -5,14 +5,17 @@ A set of custom tools to handle several useful actions (well, at least useful fo
 
 ## Resources
 see : 
-  - https://zim-wiki.org/manual/Help/Custom_Tools.html
+  - https://zim-wiki.org/manual/Help/Custom_Tools.html for information about how to create new tools in Zim
   
-
 ## Tools
 - pdf_convert
   - convert a page to a pdf document
-  - python3 /home/mist/workspace/zim_plugins/pdf_convert.py %f %p
+  - python3 ~/workspace/zim_plugins/pdf_convert.py %f pdf_convert
 
 - remarkable
   - send a file to remarkable
-  - python3 /home/mist/workspace/zim_plugins/pdf_convert.py %f %p 1
+  - python3 /home/mist/workspace/zim_plugins/pdf_convert.py %f remarkable
+
+- remarkableS
+  - send a file to remarkable and let space in every subsections
+  - python3 /home/mist/workspace/zim_plugins/pdf_convert.py %f remarkableS
--- pdf_convert.py
+++ pdf_convert.py
@@ -1,3 +1,12 @@
+"""
+This script should be called by Zim as a plugin.
+it can be used in several tools :
+- pdf_convert: convert a page to a pdf. This use Zim libraries to convert from wiki to markdown, 
+  then Pandoc to convert to pdf.
+- remarkable: convert a page to a pdf, then do a request to send the file to a connected remarkable
+- remarkableS: convert a page to a pdf with spacing before every subsections to have space to write on the remarkable, 
+  then do a request to send the file to a connected remarkable
+"""
 import os
 import sys
 import logging
@@ -11,7 +20,7 @@ from zim.formats import ParseTreeBuilder, StubLinker
 
 
 LOGNAME = "/tmp/pdf_convert.log"
-LEVEL = logging.DEBUG
+LOGLEVEL = logging.DEBUG
 DIRNAME = "/tmp/zim_pdf_convert"
 MARKDOWN_PATH = os.path.join(DIRNAME, 'markdown.md')
 
@@ -19,14 +28,10 @@ logging.basicConfig(filename=LOGNAME,
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
-                    level=LEVEL)
+                    level=LOGLEVEL)
 
 
-def main(source_file, name, remarkable):
-  # Quick sanity check
-  name = name.split(':')[-1]
-  name = name.replace(' ', '_')
-  
+def main(source_file, tool):
   # Open wiki file and transform it to markdown
   try:
     with open(source_file, 'r') as f:
@@ -35,43 +40,35 @@ def main(source_file, name, remarkable):
     WikiParser()(builder, text)
     md = Dumper(linker=StubLinker()).dump(builder.get_parsetree())
   except Exception as e:
-    logging.error(e)
+    logging.error(f"A critical error occurs when converting the wiki to markdown: {e}")
     return
+  logging.info("conversion to markdown done")  
 
   # write markdown to tmp file
   os.makedirs(DIRNAME, exist_ok=True)
   with open(MARKDOWN_PATH, 'w') as f:
+    # the first line of the markdown define the name
+    firstline = f.readline()
+    name = firstline.replace(' ', '_').replace('\n', '')
+    f.write(firstline)
     for line in md:
       f.write(line)
 
-  with open(MARKDOWN_PATH, 'r') as f:
-    name = f.readline()
-    name = name.replace(' ', '_')
-    name = name.replace('\n', '')
-  
   variables = "--variable fontsize=12pt"
-  if remarkable:  
+  if tool[-1] == 'S':
     variables += " --variable splitsection=1"
-    
-  
 
   # Call pandoc to convert this file to pdf
   pdf_path = os.path.join(DIRNAME, f'{name}.pdf')
   cmd = f'pandoc -N --template={pathlib.Path(__file__).parent.absolute()}/template.tex --variable geometry=a4paper,left=2cm,right=2cm,top=2cm,bottom=2cm {variables} {MARKDOWN_PATH} --pdf-engine=xelatex -o {pdf_path}'
-  logging.info(cmd)
+  logging.info(f"execure command: {cmd}")
   e = subprocess.run(cmd.split(' '))
   logging.info(e)
   subprocess.run(['nautilus', DIRNAME])
 
+
 if __name__ == "__main__":
-  f = sys.argv[1]  # %f in vim notation
-  p = sys.argv[2]  # %p in vim notation
-  remarkable = len(sys.argv) > 3
-  logging.info("new convert")
-  logging.info("called with")
-  logging.info(f"f: {f}")
-  logging.info(f"p: {p}")
-  logging.info(f"remarkable: {remarkable}")
-  logging.info(sys.argv)
-  logging.info(len(sys.argv))
-  main(f, p, remarkable)
+  f = sys.argv[1]  # %f in vim notation, path of the source file we need to work on
+  tool = sys.argv[3]  # name of the plugin that called this file. Can be pdf_convert, remarkable, remarkableS
+  logging.info(f"new convert called with, f: {f}, tool: {tool}")
+  main(f, tool)
