d7e79d9bd8cfa775ae96078c2fe885935d44828e
Sebastien Iooss <sebastien.iooss@upstride.io>
2021-01-17 12:37:56 +0100

    better code block support

--- Readme.md
+++ Readme.md
@@ -3,6 +3,11 @@
 A set of custom tools to handle several useful actions (well, at least useful for me...)
 
 
+## Notes
+  - it seems the wiki parser of zim 0.72.0 is broken (this is the default version in Ubuntu 20.04). Please follow 2.0.2 of https://doc.ubuntu-fr.org/zim#installation
+  to install it from the official PPA 
+  
+
 ## Resources
 see : 
   - https://zim-wiki.org/manual/Help/Custom_Tools.html for information about how to create new tools in Zim
@@ -10,12 +15,12 @@ see :
 ## Tools
 - pdf_convert
   - convert a page to a pdf document
-  - python3 ~/workspace/zim_plugins/pdf_convert.py %f pdf_convert
+  - python3 PATH_TO_THIS_PROJECT/pdf_convert.py %f pdf_convert
 
 - remarkable
   - send a file to remarkable
-  - python3 /home/mist/workspace/zim_plugins/pdf_convert.py %f remarkable
+  - python3 PATH_TO_THIS_PROJECT/pdf_convert.py %f remarkable
 
 - remarkableS
   - send a file to remarkable and let space in every subsections
-  - python3 /home/mist/workspace/zim_plugins/pdf_convert.py %f remarkableS
+  - python3 PATH_TO_THIS_PROJECT/pdf_convert.py %f remarkableS
--- pdf_convert.py
+++ pdf_convert.py
@@ -1,16 +1,17 @@
 """
 This script should be called by Zim as a plugin.
 it can be used in several tools :
-- pdf_convert: convert a page to a pdf. This use Zim libraries to convert from wiki to markdown, 
+- pdf_convert: convert a page to a pdf. This use Zim libraries to convert from wiki to markdown,
   then Pandoc to convert to pdf.
 - remarkable: convert a page to a pdf, then do a request to send the file to a connected remarkable
-- remarkableS: convert a page to a pdf with spacing before every subsections to have space to write on the remarkable, 
+- remarkableS: convert a page to a pdf with spacing before every subsections to have space to write on the remarkable,
   then do a request to send the file to a connected remarkable
 """
 import os
 import sys
 import logging
 import subprocess
+import traceback
 import pathlib
 
 # We will use official zim tools to convert the wiki to a markdown file
@@ -24,11 +25,25 @@ LOGLEVEL = logging.DEBUG
 DIRNAME = "/tmp/zim_pdf_convert"
 MARKDOWN_PATH = os.path.join(DIRNAME, 'markdown.md')
 
-logging.basicConfig(filename=LOGNAME,
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
+                    handlers=[logging.FileHandler(LOGNAME), logging.StreamHandler()],
                     level=LOGLEVEL)
+
+
+class CustomDumper(Dumper):
+  def dump_object_fallback(self, tag, attrib, strings=None):
+
+    # dump code as... code
+    if attrib['type'] == 'code':
+      lang = attrib['lang']
+      out = [f"```{lang}\n"] \
+          + self.prefix_lines('', strings) \
+          + [f"```\n"]
+      return out
+
+    # dump object as verbatim block
+    return self.prefix_lines('\t', strings)
 
 
 def main(source_file, tool):
@@ -38,19 +53,20 @@ def main(source_file, tool):
       text = f.read()
     builder = ParseTreeBuilder()
     WikiParser()(builder, text)
-    md = Dumper(linker=StubLinker()).dump(builder.get_parsetree())
+    md = CustomDumper(linker=StubLinker()).dump(builder.get_parsetree())
   except Exception as e:
+    tb = traceback.format_exc()
     logging.error(f"A critical error occurs when converting the wiki to markdown: {e}")
+    logging.error(f"{tb}")
     return
-  logging.info("conversion to markdown done")  
+  logging.info("conversion to markdown done")
+
+  # get name
+  name = md[0].replace(' ', '_').replace('\n', '')
 
   # write markdown to tmp file
   os.makedirs(DIRNAME, exist_ok=True)
   with open(MARKDOWN_PATH, 'w') as f:
-    # the first line of the markdown define the name
-    firstline = f.readline()
-    name = firstline.replace(' ', '_').replace('\n', '')
-    f.write(firstline)
     for line in md:
       f.write(line)
 
@@ -68,7 +84,11 @@ def main(source_file, tool):
 
 
 if __name__ == "__main__":
-  f = sys.argv[1]  # %f in vim notation, path of the source file we need to work on
-  tool = sys.argv[3]  # name of the plugin that called this file. Can be pdf_convert, remarkable, remarkableS
-  logging.info(f"new convert called with, f: {f}, tool: {tool}")
-  main(f, tool)
+  try:
+    f = sys.argv[1]  # %f in vim notation, path of the source file we need to work on
+    tool = sys.argv[2]  # name of the plugin that called this file. Can be pdf_convert, remarkable, remarkableS
+    logging.info(f"new convert called with, f: {f}, tool: {tool}")
+    main(f, tool)
+  except Exception as e:
+    tb = traceback.format_exc()
+    logging.error(f"{tb}")
